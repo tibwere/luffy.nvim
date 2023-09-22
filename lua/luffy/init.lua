e287df8367eeee053c0f524a3988a020c75e127c
Simone Tiberi <tib.sim.98@gmail.com>
2023-09-22 18:13:16 +0200

    util: added an helper function that lets you to safely require a module

--- lua/luffy/init.lua
+++ lua/luffy/init.lua
@@ -1,9 +1,23 @@
 local M = {}
 
+M.plugin_name = "luffy"
+
 function M.emit_notify(title, body, level, timeout)
   level = level or vim.log.levels.INFO
   timeout = timeout or 1000
   vim.notify(body, level, { title = title, timeout = 1000 })
+end
+
+function M.safe_require(module)
+  local ok, ret = pcall(require, M.plugin_name .. "." .. module)
+  if not ok then
+    M.emit_notify(
+      "Module load",
+      "An error was encountered while trying to load '" .. module .. "'"
+    )
+  end
+
+  return ret
 end
 
 function M.setup()
