b58f8c4a5f527f5c60a606815e36bc7ff324dbf5
Simone Tiberi <tib.sim.98@gmail.com>
2023-09-22 22:40:15 +0200

    autocmd: added a new autocmd for the luffy group to highlight yanked text

--- lua/luffy/auto.lua
+++ lua/luffy/auto.lua
@@ -1,3 +1,14 @@
 local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
 
 local luffygrp = augroup("luffy", { clear = true })
+
+autocmd({ "TextYankPost" }, {
+  group = luffygrp,
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",
+      timeout = 500,
+    })
+  end,
+})
