09a80dd85b6a99339bcc19d481e69858cb12fd7b
Simone Tiberi <tib.sim.98@gmail.com>
2023-09-22 18:11:57 +0200

    util: added an helper function that lets you to emit a vim notification with good defaults

--- lua/luffy/init.lua
+++ lua/luffy/init.lua
@@ -1,5 +1,11 @@
 local M = {}
 
+function M.emit_notify(title, body, level, timeout)
+  level = level or vim.log.levels.INFO
+  timeout = timeout or 1000
+  vim.notify(body, level, { title = title, timeout = 1000 })
+end
+
 function M.setup()
 end
 
