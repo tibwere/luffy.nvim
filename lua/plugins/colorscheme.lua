781ba80030c27f921af1833426246f160b5b8e7b
Simone Tiberi <tib.sim.98@gmail.com>
2023-09-24 15:57:08 +0200

    plugin: added gruvbox plugin suport

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,10 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  lazy = true,
-  opts = { flavour = "mocha" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = true,
+    opts = { flavour = "mocha" },
+  },
+  { "ellisonleao/gruvbox.nvim", priority = 1000, lazy = true },
 }
