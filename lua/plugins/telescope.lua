a9f5b71fb89f6369e2b3116b30213c544efdf1f9
Simone Tiberi <tib.sim.98@gmail.com>
2023-09-24 15:57:37 +0200

    plugin: added telescope plugin

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    vim.keymap.set("n", "<leader>fc", builtin.colorscheme, {})
+    vim.keymap.set("n", "<C-f>", function()
+      builtin.grep_string({ search = vim.fn.input("Searching for: ") })
+    end)
+  end,
+}
