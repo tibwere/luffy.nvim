fef3e0bf5df48e0a7bb3961205de268b99963c82
Simone Tiberi <tib.sim.98@gmail.com>
2023-09-25 09:22:40 +0200

    plugin: added descriptions to the telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,14 +2,15 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
+    local map = vim.keymap.set
     local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-    vim.keymap.set("n", "<leader>fc", builtin.colorscheme, {})
-    vim.keymap.set("n", "<C-f>", function()
+    map("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+    map("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+    map("n", "<leader>fb", builtin.buffers, { desc = "Open buffers" })
+    map("n", "<leader>fh", builtin.help_tags, { desc = "Search an help page" })
+    map("n", "<leader>fc", builtin.colorscheme, { desc = "Set a colorscheme" })
+    map("n", "<C-f>", function()
       builtin.grep_string({ search = vim.fn.input("Searching for: ") })
-    end)
+    end, { desc = "Search for" })
   end,
 }
