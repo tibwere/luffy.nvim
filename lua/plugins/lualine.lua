098c495e58f16304232cb7e1851139b8eec32b69
Simone Tiberi <tib.sim.98@gmail.com>
2023-09-25 22:37:08 +0200

    plugin: added an helper function to be used in lualine components to conditionally display them

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,9 @@
+-- helper function that is used to choose
+-- if show component on the lualine or not
+local conditional_hide = function()
+  return vim.fn.winwidth(0) > 120
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
