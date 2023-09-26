b3689de96aca895f57eb01821f77aa93518fec2d
Simone Tiberi <tib.sim.98@gmail.com>
2023-09-26 09:21:42 +0200

    keymap: added desc field to gitsigns keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -20,7 +20,7 @@ return {
           gs.next_hunk()
         end)
         return "<Ignore>"
-      end, { expr = true })
+      end, { expr = true, desc = "Next hunk" })
 
       map("n", "[c", function()
         if vim.wo.diff then
@@ -30,30 +30,35 @@ return {
           gs.prev_hunk()
         end)
         return "<Ignore>"
-      end, { expr = true })
+      end, { expr = true, desc = "Previous hunk" })
 
       -- Actions
-      map("n", "<leader>hs", gs.stage_hunk)
-      map("n", "<leader>hr", gs.reset_hunk)
+      map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage hunk (normal)" })
+      map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset hunk (normal)" })
       map("v", "<leader>hs", function()
         gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      end)
+      end, { desc = "Stage hunk (visual)" })
       map("v", "<leader>hr", function()
         gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      end)
-      map("n", "<leader>hS", gs.stage_buffer)
-      map("n", "<leader>hu", gs.undo_stage_hunk)
-      map("n", "<leader>hR", gs.reset_buffer)
-      map("n", "<leader>hp", gs.preview_hunk)
+      end, { desc = "Reset hunk (visual)" })
+      map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage buffer" })
+      map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Undo stage hunk" })
+      map("n", "<leader>hR", gs.reset_buffer, { desc = "Reset buffer" })
+      map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview hunk" })
       map("n", "<leader>hb", function()
         gs.blame_line({ full = true })
-      end)
-      map("n", "<leader>tb", gs.toggle_current_line_blame)
-      map("n", "<leader>hd", gs.diffthis)
+      end, { desc = "Line blame (full)" })
+      map(
+        "n",
+        "<leader>tb",
+        gs.toggle_current_line_blame,
+        { desc = "Toggle current line blame" }
+      )
+      map("n", "<leader>hd", gs.diffthis, { desc = "Open diff" })
       map("n", "<leader>hD", function()
         gs.diffthis("~")
-      end)
-      map("n", "<leader>td", gs.toggle_deleted)
+      end, { desc = "Open diff with HEAD~" })
+      map("n", "<leader>td", gs.toggle_deleted, { desc = "Toggle deleted" })
     end,
   },
 }
