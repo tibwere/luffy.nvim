667f5c8b363f653495254f906b0fec25d6fc1155
Simone Tiberi <tib.sim.98@gmail.com>
2023-10-01 23:55:58 +0200

    devel: manually added keybinds (so now they are integrated with which-key)

--- lua/luffy/devel.lua
+++ lua/luffy/devel.lua
@@ -1,7 +1,36 @@
 local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(_, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
+  local function map(mode, lhs, rhs, opts)
+    local bufopt = { buffer = bufnr }
+    local options = vim.tbl_deep_extend("force", bufopt, opts or {})
+    vim.keymap.set(mode, lhs, rhs, options)
+  end
+
+  map("n", "gD", vim.lsp.buf.declaration, { desc = "Goto declaration" })
+  map("n", "gd", vim.lsp.buf.definition, { desc = "Goto definition" })
+  map("n", "K", vim.lsp.buf.hover, { desc = "Symbol informations" })
+  map("n", "gi", vim.lsp.buf.implementation, { desc = "List implementations" })
+  map(
+    "n",
+    "<C-k>",
+    vim.lsp.buf.signature_help,
+    { desc = "Signature informations" }
+  )
+  map(
+    "n",
+    "go",
+    vim.lsp.buf.type_definition,
+    { desc = "Goto the type definition" }
+  )
+  map("n", "gr", vim.lsp.buf.references, { desc = "List references" })
+  map("n", "<F2>", vim.lsp.buf.rename, { desc = "Rename all references" })
+  map({ "n", "x" }, "<F3>", function()
+    vim.lsp.buf.format({ async = true })
+  end, { desc = "Format the buffer" })
+  map("n", "<F4>", vim.lsp.buf.code_action, { desc = "Select an action" })
+  map("n", "]d", vim.diagnostic.goto_next, { desc = "Goto next diagnostic" })
+  map("n", "[d", vim.diagnostic.goto_prev, { desc = "Goto next diagnostic" })
 end)
 
 require("mason-lspconfig").setup({
