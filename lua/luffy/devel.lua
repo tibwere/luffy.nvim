4ce1da19a686340ebfa4f50638bc8bbb6409ea27
Simone Tiberi <tib.sim.98@gmail.com>
2023-10-02 00:34:39 +0200

    devel: added IntellJ-like tab completion

--- lua/luffy/devel.lua
+++ lua/luffy/devel.lua
@@ -121,6 +121,21 @@ cmp.setup({
     { name = "path" },
   }),
   mapping = {
+    -- IntellJIDEA like tab completion
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+      if cmp.visible() then
+        local entry = cmp.get_selected_entry()
+        if not entry then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        else
+          cmp.confirm()
+        end
+      else
+        fallback()
+      end
+    end, { "i", "s", "c" }),
+
     -- `Enter` key to confirm completion
     ["<CR>"] = cmp.mapping.confirm({ select = false }),
 
