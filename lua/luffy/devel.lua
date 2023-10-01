8a5d30273f119d9715658a757d7db681da8eadcf
Simone Tiberi <tib.sim.98@gmail.com>
2023-10-02 00:04:38 +0200

    devel: added a specific configuration for lua_ls server (support for neovim env.)

--- lua/luffy/devel.lua
+++ lua/luffy/devel.lua
@@ -53,5 +53,11 @@ require("mason-lspconfig").setup({
   ensure_installed = servers,
   handlers = {
     lsp_zero.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls({
+        telemetry = { enable = false },
+      })
+      require("lspconfig").lua_ls.setup(lua_opts)
+    end,
   },
 })
