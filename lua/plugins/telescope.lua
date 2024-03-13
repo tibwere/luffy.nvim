8819424e9cbfacf8d94810807d51c2de753291cc
Simone Tiberi <tib.sim.98@gmail.com>
2024-03-13 09:28:00 +0100

    plugin: removed C-f remap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,8 +26,5 @@ return {
     map("n", "<leader>fb", builtin.buffers, { desc = "Open buffers" })
     map("n", "<leader>fh", builtin.help_tags, { desc = "Search an help page" })
     map("n", "<leader>fc", builtin.colorscheme, { desc = "Set a colorscheme" })
-    map("n", "<C-f>", function()
-      builtin.grep_string({ search = vim.fn.input("Searching for: ") })
-    end, { desc = "Search for" })
   end,
 }
