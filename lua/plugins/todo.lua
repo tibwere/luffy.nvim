9071609682fec98dd782b0d002920dbc63e0fd69
Simone Tiberi <tib.sim.98@gmail.com>
2024-09-12 18:03:16 +0200

    plugin: todo: add telescope support

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -2,6 +2,14 @@ return {
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {}
+    config = function()
+      require("todo-comments").setup({})
+      vim.keymap.set(
+        "n",
+        "<leader>ft",
+        "<cmd>TodoTelescope<cr>",
+        { desc = "Open Todo list in telescope" }
+      )
+    end,
   },
 }
