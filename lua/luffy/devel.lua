c7847bf9358025782af2f2491823f1d7199b2588
Simone Tiberi <tib.sim.98@gmail.com>
2024-09-26 10:45:13 +0200

    devel: remove cspell

--- lua/luffy/devel.lua
+++ lua/luffy/devel.lua
@@ -236,18 +236,10 @@ else
 end
 
 local null_ls = require("null-ls")
-local cspell = require("cspell")
 
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.shfmt,
-    cspell.diagnostics.with({
-      filetypes = { "text", "markdown" },
-      diagnostics_postprocess = function(diagnostic)
-        diagnostic.severity = vim.diagnostic.severity["HINT"]
-      end,
-    }),
-    cspell.code_actions.with({ filetypes = { "text", "markdown" } }),
   },
 })
