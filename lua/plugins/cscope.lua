c98834acbf8d0913062d99908b12f1576a1f2646
Simone Tiberi <tib.sim.98@gmail.com>
2025-02-11 16:13:59 +0100

    plugin: update all plugins

--- lua/plugins/cscope.lua
+++ lua/plugins/cscope.lua
@@ -12,7 +12,7 @@ return {
       -- b: build the database only (i.e., not launch the GUI)
       -- q: enable fast symbol lookup (i.e., .in.out and .po.out created)
       -- k: kernel mode (disable the default include dir /usr/include)
-      db_build_cmd_args = { "-Rbqk" },
+      db_build_cmd = { args = {"-Rbqk"} },
       skip_picker_for_single_result = true,
     },
   },
