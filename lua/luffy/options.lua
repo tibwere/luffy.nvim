042f08b0d517341af17a32848bd65382232de4bb
Simone Tiberi <tib.sim.98@gmail.com>
2026-01-29 17:05:27 +0100

    core: add stuff from nvim-lua/kickstart.nvim

--- lua/luffy/options.lua
+++ lua/luffy/options.lua
@@ -25,6 +25,16 @@ local options = {
   signcolumn = "yes",
   timeout = true,
   timeoutlen = 300,
+
+  -- From nvim-lua/kickstart.nvim
+  -- Preview substitutions live, as you type!
+  inccommand = "split",
+  -- Show which line your cursor is on
+  cursorline = true,
+  -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+  -- instead raise a dialog asking if you wish to save the current file(s)
+  -- See `:help 'confirm'`
+  confirm = true,
 }
 
 for k, v in pairs(options) do
