f2e5b0dfd38149a27ddc0e4849beef99471d52dd
Simone Tiberi <tib.sim.98@gmail.com>
2026-01-30 10:05:33 +0100

    plugins: add telescope related keymaps from kickstart.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,5 +26,27 @@ return {
     map("n", "<leader>fb", builtin.buffers, { desc = "Open buffers" })
     map("n", "<leader>fh", builtin.help_tags, { desc = "Search an help page" })
     map("n", "<leader>fc", builtin.colorscheme, { desc = "Set a colorscheme" })
+
+    -- From nvim-lua/kickstart.nvim
+    map("n", "<leader>/", function()
+      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+      builtin.current_buffer_fuzzy_find(
+        require("telescope.themes").get_dropdown({
+          winblend = 10,
+          previewer = false,
+        })
+      )
+    end, { desc = "[/] Fuzzily search in current buffer" })
+    map("n", "<leader>s/", function()
+      builtin.live_grep({
+        grep_open_files = true,
+        prompt_title = "Live Grep in Open Files",
+      })
+    end, { desc = "[S]earch [/] in Open Files" })
+
+    -- Shortcut for searching your Neovim configuration files
+    map("n", "<leader>sn", function()
+      builtin.find_files({ cwd = vim.fn.stdpath("config") })
+    end, { desc = "[S]earch [N]eovim files" })
   end,
 }
