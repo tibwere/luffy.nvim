6228a3a14cc8fdc9de09000b0ea45778508978d7
Simone Tiberi <tib.sim.98@gmail.com>
2026-02-16 16:41:47 +0100

    plugins: add <leader>fG shortcut to grep for current word

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -23,6 +23,7 @@ return {
     local builtin = require("telescope.builtin")
     map("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
     map("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+    map("n", "<leader>fG", builtin.grep_string, {desc = "Search current word"})
     map("n", "<leader>fb", builtin.buffers, { desc = "Open buffers" })
     map("n", "<leader>fh", builtin.help_tags, { desc = "Search an help page" })
     map("n", "<leader>fc", builtin.colorscheme, { desc = "Set a colorscheme" })
