f8e45c1f4050ff683114ee091555c936c7535723
Simone Tiberi <tib.sim.98@gmail.com>
2026-02-24 11:35:09 +0100

    options: add diagnostic config from kickstart

--- lua/luffy/options.lua
+++ lua/luffy/options.lua
@@ -60,3 +60,18 @@ vim.fn.matchadd("ExtraWhitespace", [[\s\+$]])
 vim.schedule(function()
   vim.o.clipboard = "unnamedplus"
 end)
+
+-- Diagnostic Config
+vim.diagnostic.config({
+  update_in_insert = false,
+  severity_sort = true,
+  float = { border = "rounded", source = "if_many" },
+  underline = { severity = vim.diagnostic.severity.ERROR },
+
+  -- Can switch between these as you prefer
+  virtual_text = true, -- Text shows up at the end of the line
+  virtual_lines = false, -- Teest shows up underneath the line, with virtual lines
+
+  -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
+  jump = { float = true },
+})
