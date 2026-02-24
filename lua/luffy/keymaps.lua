10c929778e3c44241cc81e9e539d3c5f07b79918
Simone Tiberi <tib.sim.98@gmail.com>
2026-02-24 11:45:57 +0100

    keymaps: add keymap for quicklist

--- lua/luffy/keymaps.lua
+++ lua/luffy/keymaps.lua
@@ -16,6 +16,13 @@ vim.g.maplocaleader = " "
 
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+map(
+  "n",
+  "<leader>q",
+  vim.diagnostic.setloclist,
+  { desc = "Open diagnostic quickfix list" }
+)
+
 map("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 map("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 map("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
